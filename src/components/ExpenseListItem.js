705ee7a9a75cd76ee6048f9f375de87017d4e49c
Tim DeWolf <tim@tdewolf.net>
2018-10-25 14:28:02 -0600

    Add expense summary to dashboard

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -10,7 +10,7 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
         <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
         <p>{numeral(amount/100).format('$0,0.00')}
         -
-        {moment(createdAt).format('MMMM D, YYYY')}</p>
+        {moment(createdAt).format('MMMM Do, YYYY')}</p>
     </div>
 );
 
--- src/tests/components/ExpenseList.test.js
+++ src/tests/components/ExpenseList.test.js
@@ -6,7 +6,7 @@ import expenses from '../fixtures/expenses';
 test('should render ExpenseList with expenses', () => {
     const wrapper = shallow(<ExpenseList expenses={expenses} />);
     expect(wrapper).toMatchSnapshot();
-})
+});
 
 test('should render ExpenseList with empty message', () => {
     const wrapper = shallow(<ExpenseList expenses={[]} />);
