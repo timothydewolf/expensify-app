dc08c7359dd1477ba2c0cf48868e21a660073f63
Tim DeWolf <tim@tdewolf.net>
2018-11-07 11:38:48 -0700

    Get firebase expenses on start

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,7 +9,7 @@ projectId: process.env.FIREBASE_PROJECT_ID,
 storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
-console.log(config.databaseURL);
+//console.log(config.databaseURL);
 firebase.initializeApp(config);
 
 const database = firebase.database();
